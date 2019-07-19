49ba1ca93ce5e7b4f53ab3260312bba4a0e6a2e3
github username <github email address>
2019-07-18 23:20:20 -0400

    first int

---
+++ src/containers/BreweryContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class BreweryContainer extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>Hello World</div>
+         );
+    }
+}
+ 
+export default BreweryContainer;
