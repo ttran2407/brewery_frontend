e1e3f707643498f52f3e1073f52f3f9b909fce3f
github username <github email address>
2019-07-22 08:30:57 -0400

    Fixing Typo

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -10,16 +10,16 @@ const Intro = (props) => {
 
                 
                     <div className="first">
-                        We are a group of Engineer sharing a same passion with craft beer.
-                        We're proud to provide 350+ breweries locations across Pensylvania. 
+                        We are a group of Engineers sharing the same passion with craft beer.
+                        We're proud to provide 350+ brewery locations across Pennsylvania. 
                     </div>
                     <div className="second">
-                        To find your favorite brewery, simple write the brewery's name (if you know) on the search box above. 
-                        The result is display below. 
-                        If you don't remember the name. That's fine. Their are 350+ locations in the table below.
+                        To find your favorite brewery, simply write the brewery's name (if you know) on the search box above. 
+                        The result will be displayed below. 
+                        If you don't remember the name, that's fine. There are 350+ locations in the table below.
                     </div>
                     <div className="third">
-                        We believe a good drink is a key for a happly life. Happy drinking!
+                        We believe a good drink is the key to a happly life. Happy drinking!
                     </div>
                 
             </div>
